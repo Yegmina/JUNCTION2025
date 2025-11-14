0615137e4a03457785eb609ac1226c7846b08b94
Yehor Tereshchenko <123106244+Yegmina@users.noreply.github.com>
2025-11-14 23:47:15 +0200

    Add Wolt video share demo with Flutter web implementation

---
+++ lib/widgets/delivery_info_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class DeliveryInfoWidget extends StatelessWidget {
+  final int deliveryTimeMinutes;
+  final double distanceKm;
+
+  const DeliveryInfoWidget({
+    super.key,
+    required this.deliveryTimeMinutes,
+    required this.distanceKm,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(
+          Icons.access_time,
+          size: 14,
+          color: Colors.grey[600],
+        ),
+        const SizedBox(width: 4),
+        Text(
+          '$deliveryTimeMinutes min',
+          style: TextStyle(
+            fontSize: 12,
+            color: Colors.grey[700],
+          ),
+        ),
+        const SizedBox(width: 12),
+        Icon(
+          Icons.location_on,
+          size: 14,
+          color: Colors.grey[600],
+        ),
+        const SizedBox(width: 4),
+        Text(
+          '${distanceKm.toStringAsFixed(1)} km',
+          style: TextStyle(
+            fontSize: 12,
+            color: Colors.grey[700],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
